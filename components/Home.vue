6ae9e0b36fec0861d55c0eabbecb9b7201c64ac9
Jayson Mulwa <jayson.mulwa@gmail.com>
2023-04-10 18:50:50 +0300

    feat: wip

---
+++ components/Home.vue
@@ -0,0 +1,134 @@
+<!-- Please remove this file from your project -->
+<template>
+    <section class="text-gray-600 body-font">
+      <!-- <div class="container px-5 pt-12 mx-auto">
+        <div class="flex flex-wrap w-full flex-row justify-between">
+          <span class="flex flex-row">
+            <NuxtLink to="/profile">
+            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </div>
+            </NuxtLink>
+          </span>
+          <span>
+            <NuxtLink to="/login">
+            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+              </svg>
+            </div>
+            </NuxtLink>
+          </span>
+        </div>
+      </div> -->
+    <div class="container px-5 pt-12 pb-24 mx-auto">
+      <!-- <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Hospital Management System.</h1>
+        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Health Matters.</p>
+      </div> -->
+      <div class="flex flex-wrap -m-4">
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <NuxtLink to="/patients">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                </svg>
+  
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Patients</h2>
+              <p class="leading-relaxed text-base w-full">Check Patients Informationat the click of a button.</p>
+            </div>
+          </NuxtLink>	
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <NuxtLink to="/appointments">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                </svg>
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Appointments</h2>
+              <p class="leading-relaxed text-base w-full">Quickly book and check appointments.</p>
+            </div>
+          </NuxtLink>
+        </div>
+        
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <NuxtLink to="/payments">
+          <div class="border border-gray-200 p-6 rounded-lg">
+            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+  
+            </div>
+            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Payments</h2>
+            <p class="leading-relaxed text-base w-full">Check payments received for services rendered.</p>
+          </div>
+          </NuxtLink>
+        </div>
+  
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <NuxtLink to="/staff">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                </svg>
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">All Staff</h2>
+              <p class="leading-relaxed text-base w-full">Doctors, Nurses and everyone else under one roof.</p>
+            </div>
+          </NuxtLink>
+        </div>
+  
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <NuxtLink to="/doctors">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Doctors</h2>
+              <p class="leading-relaxed text-base w-full">For quick and easy management of doctors.</p>
+            </div>
+          </NuxtLink>
+        </div>
+  
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <NuxtLink to="/nurses">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                </svg>
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Nurses</h2>
+              <p class="leading-relaxed text-base w-full">For quick and easy management of nurses.</p>
+            </div>
+          </NuxtLink>
+        </div>
+  
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <NuxtLink to="/departments">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+                </svg>
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Departments</h2>
+              <p class="leading-relaxed text-base w-full">Management for all hospital departments.</p>
+            </div>
+          </NuxtLink>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+  </template>
---
+++ pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <Navbar />
+    <Home />
+    <Footer />
+  </template>
